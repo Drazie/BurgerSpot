2f7904d8fdf6848145dab2e449103b3ddf992dd9
Drazie <54319841+Drazie@users.noreply.github.com>
2021-02-28 22:36:02 +0200

    MVVM practice

---
+++ BurgerSpot/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BurgerSpot.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        
+    }
+}
